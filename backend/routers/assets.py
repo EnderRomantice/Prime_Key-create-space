00eeec655354ac22bf0beb4a3e1087a053b81a6f
rustlove <enderromantic@gmail.com>
2025-03-02 19:07:48 +0800

    backend URL reload

--- backend/routers/assets.py
+++ backend/routers/assets.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
+import os
+
+URL = os.getcwd()
 
 router = APIRouter()
 @router.get("/assets/{fileName}")
 async def getP1(fileName :str):
     #获取资源
-    return FileResponse(f"E:/forum-project/backend/assets/{fileName}")
+    return FileResponse(f"{URL}/assets/{fileName}")
--- backend/tools/link_db.py
+++ backend/tools/link_db.py
@@ -1,7 +1,7 @@
 import sqlite3
 # python已内置sqlite3的驱动
 def get_db_connection():
-    conn = sqlite3.connect('E:\\forum-project\\backend\\database.db')
+    conn = sqlite3.connect('C:\\projects\\Prime_Key-create-space\\backend\\database.db')
     conn.row_factory = sqlite3.Row
 
     return conn
